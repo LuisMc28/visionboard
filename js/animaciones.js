1912ab26e811d269a825715a9efa5e8bd98b034d
SiulMc28 <lmedinacabrera28@gmail.com>
2025-12-27 11:20:48 -0400

    vamoo

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,13 +1,15 @@
 const images = document.querySelectorAll('.image-box');
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
         if (entry.isIntersecting) {
+            entry.target.style.transitionDelay = `${index * 0.15}s`;
             entry.target.classList.add('show');
+            observer.unobserve(entry.target);
         }
     });
 }, {
-    threshold: 0.2
+    threshold: 0.25
 });
 
 images.forEach(img => observer.observe(img));
