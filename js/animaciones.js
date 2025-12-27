70327c15f17e9a93ed8cc2335a6e3f890a043e34
SiulMc28 <lmedinacabrera28@gmail.com>
2025-12-27 11:05:37 -0400

    cambioss

---
+++ js/animaciones.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll('.image-box');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+images.forEach(img => observer.observe(img));
